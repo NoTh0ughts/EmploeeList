97c37cd9d18ebd1d73e90201dee858136ab02096
No_thoughts <89064032445@mail.ru>
2024-07-24 22:18:01 +0300

    Changed IRepository signature by default CRUD returns value

--- EmployeeList/DataProvider/IRepository.cs
+++ EmployeeList/DataProvider/IRepository.cs
@@ -3,8 +3,8 @@
 public interface IRepository<T>
 {
     public IEnumerable<T> GetAll();
-    public T Get   (int id);
-    public T Update(int id, T item);
-    public T Delete(int id);
-    public T Add   (T item);
+    public T? Get   (int id);
+    public T? Update(int id, T item);
+    public bool Delete(int id);
+    public void Add(T item);
 }
