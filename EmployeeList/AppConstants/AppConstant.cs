14ad5d658460478f2c9279fd0bbbd2f66e34fe58
No_thoughts <89064032445@mail.ru>
2024-07-26 20:16:14 +0300

    Added new ExitCode`s and Input constants

--- EmployeeList/AppConstants/AppConstant.cs
+++ EmployeeList/AppConstants/AppConstant.cs
@@ -2,9 +2,25 @@
 
 public class AppConstant
 {
+    public class Input
+    {
+        public const char ARGS_SEPARATOR = ':';
+    }
     public class ExitCodes
     {
-        public const int INVALID_PATH = 1;
+        public const int OK                = 0;
+        public const int INVALID_PATH      = 1;
+        public const int HAVE_NO_ARGS      = 2;
+        public const int INVALID_ARGS      = 3;
+        public const int UNKNOWN_COMMAND   = 5;
+        public const int ERROR             = 6;
+  
+        public const int CANNOT_ADD        = 7;
+        public const int CANNOT_UPDATE     = 8;
+        public const int CANNOT_DELETE     = 9;
+        public const int CANNOT_GET        = 10;
+        public const int CANNOT_GETALL     = 11;
+        public const int NOTHING_TO_CHANGE = 12;
         
     }
 }
