bf1d58e30953f13401a46b8aa36afb658a7d2f3e
No_thoughts <89064032445@mail.ru>
2024-07-26 20:17:50 +0300

    added Data source functionality

---
+++ EmployeeList/DataProvider/FileSource/LocalFileDataSource.cs
@@ -0,0 +1,34 @@
+﻿namespace EmployeeList.DataProvider.FileSource;
+
+/// <summary>
+/// Локальный источник данных
+/// Реализует взаимодействие с файловой системой
+/// </summary>
+public class LocalFileDataSource : IDataSource
+{
+    /// <summary>
+    /// Производит синхронное чтение файла и возвращает полученные данные
+    /// </summary>
+    /// <param name="path">Путь к файлу</param>
+    /// <returns>Данные из файла</returns>
+    /// <exception cref="FileNotFoundException">Файл не найден</exception>
+    public string GetRawData(string path)
+    {
+        if (false == File.Exists(path))
+        {
+            throw new FileNotFoundException($"The file {path} was not found.");
+        }
+
+        return File.ReadAllText(path);
+    }
+
+    /// <summary>
+    /// Сохраняет синхронно данные в файл
+    /// </summary>
+    /// <param name="data"></param>
+    /// <param name="path"></param>
+    public void Save(string path, string data)
+    {
+        File.WriteAllText(path, data);
+    }
+}
